e4ba308186609207f2a3e19ce1e77000cf7da1b2
Wilson <cusackw@gmail.com>
2015-01-28 16:39:06 -0500

    first commit

---
+++ resources/public/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#number1").on("keypress", function (e) {
+    if (e.keyCode == 13) {
+      // $.get("endPoint", dataToSend, Function to call on response)
+      $.get("/add1", {toAdd: $("#number1").val()}, function(data) {
+        $(".toRemove").remove();
+        $("#wrapper").append("<h3> Boom! </h3>")
+        $("#wrapper").append("<h3>" + data.result + "</h3>")
+      });
+    }
+  });
+});
